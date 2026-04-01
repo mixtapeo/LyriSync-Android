cac9d93be8eab606cc001cba32d37ce43aa13bf1
mixtapeo <anirudhsridhar626@gmail.com>
2026-04-01 00:56:25 -0400

    forgot to update version code lol

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.mixtapeo.lyrisync"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "v0.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
